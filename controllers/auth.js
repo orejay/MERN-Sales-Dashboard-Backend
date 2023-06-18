fa156c0a5e8fe4e4b42fa059c11b9eef69c930b1
orejay <orejay@yahoo.com>
2023-06-18 16:19:16 +0100

    auth fixed

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,16 +4,27 @@ import { StatusCodes } from "http-status-codes";
 export const login = async (req, res) => {
   try {
     const { username, password } = req.body;
-    const user = await User.find({ name: username });
-    if ((user = [])) {
+    const user = await User.find({
+      name: { $regex: `^${username}$`, $options: "i" },
+    });
+    console.log(user);
+    if (user.length < 1) {
       res
         .status(StatusCodes.NOT_FOUND)
         .json({ message: "user does not exist!" });
-    }
-    if (user[0].password !== password) {
+    } else if (user[0].password !== password) {
       res.status(StatusCodes.UNAUTHORIZED).json({ message: `Wrong password!` });
+    } else {
+      res.status(StatusCodes.OK).json({
+        message: `Welcome ${user[0].name}!`,
+        details: {
+          id: user[0]._id,
+          username: user[0].name,
+          role: user[0].role,
+          occupation: user[0].occupation,
+        },
+      });
     }
-    res.status(StatusCodes.OK).json({ message: `Welcome ${user[0].name}!` });
   } catch (error) {
     res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
   }
