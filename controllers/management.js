a41490ba78639b0dcd08f1f4bef663a4b82e5037
Oreoluwa <orejay@yahoo.com>
2023-06-18 16:10:05 +0100

    Update management.js

--- controllers/management.js
+++ controllers/management.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import User from "../models/user.js";
+import User from "../models/User.js";
 import Transaction from "../models/Transaction.js";
 import { StatusCodes } from "http-status-codes";
 
